56c9264d7783261d3e4517860361fa47d8c7fa48
root <root@silpixa00400477.ir.intel.com>
2024-02-21 15:14:17 +0000

    Adding Network Function subdirectory
    First cut of network function dummy code
    Signed-off-by: Paul Hough <paul.f.hough@intel.com>

---
+++ network_function/README.md
@@ -0,0 +1,52 @@
+Network Function Skeleton Code
+
+This code attachs to an IPU network interface via a raw socket.
+All packets received on this interface are relayed unmodified 
+to a second IPU network interface. 
+This provides a "transparent pipe" which can be extended to
+provide true "bump in the wire" network functions.
+
+To compile 
+gcc -o nf nf.c
+
+to run
+./nf
+
+To set up the test environment
+
+Add a new nsamespace
+ip netns add nfns
+ip netns add ns1
+ip netns add ns2
+
+Create veth pairs
+ip link add tap1 type veth peer name br-tap1
+ip link add tap2 type veth peer name br-tap2
+
+Push to new namespace
+ip link set tap1 netns ns1
+ip link set tap2 netns ns2
+ip link set br-tap1 netns nfns
+ip link set br-tap2 netns nfns
+
+Assign addresses
+ip netns exec ns1 ip addr add 10.1.1.4/24 dev tap1
+ip netns exec ns2 ip addr add 10.1.1.5/24 dev tap2
+
+
+Bring up a new interface
+ip netns exec nfns ip link set br-tap1 up
+ip netns exec nfns ip link set br-tap2 up
+ip netns exec ns1 ip link set tap1 up
+ip netns exec ns2 ip link set tap2 up
+
+Run code:
+ip netns exec nfns ./nf br-tap1 br-tap2
+
+Run test:
+ip netns exec ns1 ping -c1 10.1.1.5
+
+Exspected Behavior
+packet could will increment, packet output to screen, source MAC printed.
+packet relayed out second interface and visible via tcpdump command
+sudo ip netns exec ns2 tcpdump -vi tap2
---
+++ network_function/nf.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <linux/if_packet.h>
+
+void printPacket(char *packet, int length) {
+    int i;
+    for (i = 0; i < length; i++) {
+        printf("%02x ", (unsigned char)packet[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int rawSocket;
+    struct sockaddr_ll sa_in, sa_out;
+    struct ifreq ifr_in, ifr_out;
+    // Create raw socket
+    rawSocket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (rawSocket == -1) {
+        perror("Failed to create raw socket");
+        exit(1);
+    }
+    // Set the interface to receive packets
+    strncpy(ifr_in.ifr_name, "br-tap1", IFNAMSIZ);
+    if (ioctl(rawSocket, SIOCGIFINDEX, &ifr_in) == -1) {
+        perror("Failed to get interface index tap 1");
+        close(rawSocket);
+        exit(1);
+    }
+    // Set the interface to send packets
+    strncpy(ifr_out.ifr_name, "br-tap2", IFNAMSIZ);
+    if (ioctl(rawSocket, SIOCGIFINDEX, &ifr_out) == -1) {
+        perror("Failed to get interface index");
+        close(rawSocket);
+        exit(1);
+    }
+    // Bind socket to interface to receive packets
+    memset(&sa_in, 0, sizeof(struct sockaddr_ll));
+    sa_in.sll_family = AF_PACKET;
+    sa_in.sll_protocol = htons(ETH_P_ALL);
+    sa_in.sll_ifindex = ifr_in.ifr_ifindex;
+    if (bind(rawSocket, (struct sockaddr *)&sa_in, sizeof(struct sockaddr_ll)) == -1) {
+        perror("Failed to bind raw socket to interface");
+        close(rawSocket);
+        exit(1);
+    }
+    // Set socket address to send packets
+    memset(&sa_out, 0, sizeof(struct sockaddr_ll));
+    sa_out.sll_family = AF_PACKET;
+    sa_out.sll_protocol = htons(ETH_P_ALL);
+    sa_out.sll_ifindex = ifr_out.ifr_ifindex;
+    static int i = 0;
+
+    // Receive and send packets
+    while (1) {
+        char buffer[65536];
+        ssize_t length;
+
+        // Receive packet
+        length = recvfrom(rawSocket, buffer, sizeof(buffer), 0, NULL, NULL);
+	    i++;
+        printf("received frame. Sent frame count %d\n", i);
+        printf ("length = %d \n", length);
+        printPacket(buffer, length);
+
+        // Extract the Ethernet packet type
+        unsigned short etherType = ntohs(*(unsigned short *)(buffer + 12));
+        // Print the Ethernet packet type
+        printf("Ethernet Packet Type: 0x%04x\n", etherType);        
+
+        // Extract the source MAC address
+        unsigned char *sourceMac = buffer + 6;
+        // Print the source MAC address
+        printf("Source MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           sourceMac[0], sourceMac[1], sourceMac[2], sourceMac[3], sourceMac[4], sourceMac[5]);
+
+
+        if (length == -1) {
+            perror("Failed to receive packet");
+            close(rawSocket);
+            exit(1);
+        } 
+  
+
+      // Send packet
+        if (sendto(rawSocket, buffer, length, 0, (struct sockaddr *)&sa_out, sizeof(struct sockaddr_ll)) == -1) {
+            perror("Failed to send packet");
+            close(rawSocket);
+            exit(1);
+        } 
+    }
+    // Close socket
+    close(rawSocket);
+    return 0;
+}
